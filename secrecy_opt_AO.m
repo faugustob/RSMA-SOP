ef06f50ed3fde5a89f00be90c678d04384d4b7d3
faugustob <faugustobueno@gmail.com>
2026-06-16 13:18:37 -0400

    ...

--- secrecy_opt_AO.m
+++ secrecy_opt_AO.m
@@ -32,8 +32,8 @@ R = 10;
 
 m_rician = (R+1)^2/(2*R+1);
 
-N_V = 25; % number of rows of regularly arranged unit cells of RIS
-N_H = 25; % number of columns of regularly arranged unit cells of RIS
+N_V = 30; % number of rows of regularly arranged unit cells of RIS
+N_H = 30; % number of columns of regularly arranged unit cells of RIS
 Nr = N_V * N_H; % total number of unit cells of RIS
 
 d_x = floor(lambda/2 * 1000) / 1000; % horizontal size of RIS element
@@ -278,8 +278,8 @@ rho_j_xyz = [ground_users_cart,fake_eavesdroppers_xyz,eavesdroppers_xyz];
 % find out whether each receiver is on the reflect side or transmit side
 reflect = sign(RIS_normal.' * (rho_j_xyz - R_xyz));
 
-M=8;
-N=8;
+M=16;
+N=16;
 
 Nsymb = M*N; 
 
