8566fd7ccdee259ace30c86d74a689dd9c9715c7
faugustob <faugustobueno@gmail.com>
2026-06-20 11:08:01 -0400

    ...

--- secrecy_opt_AO_Noma.m
+++ secrecy_opt_AO_Noma.m
@@ -12,7 +12,7 @@ end
 
 parpool('local', numWorkers);  % Start new one with desired workers
 
-Ns = 20; % number of samples for Monte Carlo simulation
+Ns = 60; % number of samples for Monte Carlo simulation
 %rng(3);
 
 transmissionType = 'mc';
