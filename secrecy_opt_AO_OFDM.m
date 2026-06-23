b855e8d18fb202885c3b3e6ef3dd7bfa1a38d61f
faugustob <faugustobueno@gmail.com>
2026-06-22 20:43:42 -0400

    ...

--- secrecy_opt_AO_OFDM.m
+++ secrecy_opt_AO_OFDM.m
@@ -12,7 +12,7 @@ end
 
 parpool('local', numWorkers);  % Start new one with desired workers
 
-Ns = 200; % number of samples for Monte Carlo simulation
+Ns = 500; % number of samples for Monte Carlo simulation
 %rng(3);
 
 transmissionType = 'mc';
