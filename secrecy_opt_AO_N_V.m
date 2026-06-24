155f46a2c2ae5085a535e4c49056501ca6061ea2
buenof <buenof@pc-ngatched-05>
2026-06-24 09:26:26 -0400

    ...

--- secrecy_opt_AO_N_V.m
+++ secrecy_opt_AO_N_V.m
@@ -107,7 +107,7 @@ R_xyz = [0; 0; R_earth+HAP_altitude]; % location of STAR-RIS; code assumes this 
 %N_V = 20; % number of rows of regularly arranged unit cells of RIS
 N_H = 20; % number of columns of regularly arranged unit cells of RIS
 
-nV_vec = 16:2:30;
+nV_vec = 20:2:40;
 
 
 %% ===================== PRE-ALLOCATION =====================
